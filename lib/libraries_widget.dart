8baa2b381b915179d5e80bde92d902e060939654
uselesslav <wdbondarenko@gmail.com>
2020-03-30 19:36:45 +0300

    Код для экрана с списком библиотек вынесен в отдельный файл

---
+++ lib/libraries_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LibrariesPage extends StatefulWidget {
+  final String title = "Libraries";
+
+  @override
+  _LibrariesPageState createState() => _LibrariesPageState();
+}
+
+class _LibrariesPageState extends State<LibrariesPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
